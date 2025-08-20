0614876598b1aa0fed669eb47284349ef70d19ab
Mohd Noman <125533081+mohd-noman1@users.noreply.github.com>
2025-08-21 04:06:46 +0530

    Create New Year Chaos.cpp

---
+++ HackerRank/Week3/New Year Chaos.cpp
@@ -0,0 +1,10 @@
+void minimumBribes(vector<int> q) {
+    int bribes=0;
+    for(int i=q.size()-1;i>=0;i--){
+        if(q[i]-(i+1)>2){ cout<<"Too chaotic\n"; return; }
+        for(int j=max(0,q[i]-2);j<i;j++){
+            if(q[j]>q[i]) bribes++;
+        }
+    }
+    cout<<bribes<<"\n";
+}
